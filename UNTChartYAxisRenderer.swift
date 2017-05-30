319ff34a886f4a5fdbea7947fd0f999d4fb9aa95
Pavlo Shestak <pavlo.shestak@gmail.com>
2017-05-30 15:17:13 +0300

    Fixed Y axis renderer to show the price markers

--- UNTChartYAxisRenderer.swift
+++ UNTChartYAxisRenderer.swift
@@ -10,50 +10,57 @@ import Foundation
 
 public class UNTChartYAxisRenderer: YAxisRenderer {
     
-//    public var markerFont = NSUIFont.systemFontOfSize(10.0)
-//    
-//    internal override func drawYLabels(context context: CGContext, fixedPosition: CGFloat, offset: CGFloat, textAlign: NSTextAlignment) {
-//        super.drawYLabels(context: context, fixedPosition: fixedPosition, offset: offset, textAlign: textAlign)
-//        
-//        guard let yAxis = yAxis as? UNTChartYAxis else { return }
-//        
-//        for marker in yAxis.markers {
-//            if marker.enabled {
-//                self.drawMarker(marker, context: context, fixedPosition: fixedPosition, offset: offset, textAlign: textAlign)
-//            }
-//        }
-//    }
-//    
-//    private func drawMarker(marker: UNTChartAxisMarker, context: CGContext, fixedPosition: CGFloat, offset: CGFloat, textAlign: NSTextAlignment) {
-//        guard let yAxis = yAxis as? UNTChartYAxis else { return }
-//        let valueToPixelMatrix = transformer.valueToPixelMatrix
-//        
-//        var point = CGPointMake(0, CGFloat(marker.value))
-//        point = CGPointApplyAffineTransform(point, valueToPixelMatrix)
-//        point.x = fixedPosition - 5.0
-//        point.y = point.y + offset
-//        ChartUtils.drawText(
-//            context: context,
-//            text: "              ",
-//            point: point,
-//            align: textAlign,
-//            attributes: [
-//                NSBackgroundColorAttributeName: marker.backgroundColor,
-//                NSFontAttributeName: markerFont,
-//                NSForegroundColorAttributeName: marker.textColor
-//            ]
-//        )
-//        let text = marker.text ?? (yAxis.valueFormatter?.stringFromNumber(marker.value) ?? String(marker.value))
-//        ChartUtils.drawText(
-//            context: context,
-//            text: text,
-//            point: point,
-//            align: textAlign,
-//            attributes: [
-//                NSBackgroundColorAttributeName: marker.backgroundColor,
-//                NSFontAttributeName: markerFont,
-//                NSForegroundColorAttributeName: marker.textColor
-//            ]
-//        )
-//    }
+    public var markerFont = NSUIFont.systemFont(ofSize: 10.0)
+    
+    internal override func drawYLabels(
+        context: CGContext,
+        fixedPosition: CGFloat,
+        positions: [CGPoint],
+        offset: CGFloat,
+        textAlign: NSTextAlignment) {
+        
+        super.drawYLabels(context: context, fixedPosition: fixedPosition, positions:positions, offset: offset, textAlign: textAlign)
+        
+        guard let yAxis = self.axis as? UNTChartYAxis else { return }
+        
+        for marker in yAxis.markers {
+            if marker.enabled {
+                self.drawMarker(marker: marker, context: context, fixedPosition: fixedPosition, offset: offset, textAlign: textAlign)
+            }
+        }
+    }
+    
+    private func drawMarker(marker: UNTChartAxisMarker, context: CGContext, fixedPosition: CGFloat, offset: CGFloat, textAlign: NSTextAlignment) {
+        guard let yAxis = self.axis as? UNTChartYAxis,
+            let transformer = self.transformer else { return }
+        let valueToPixelMatrix = transformer.valueToPixelMatrix
+        
+        var point = CGPoint(x:0, y:CGFloat(marker.value))
+        point = point.applying(valueToPixelMatrix)
+        point.x = fixedPosition - 5
+        point.y = point.y + offset
+        ChartUtils.drawText(
+            context: context,
+            text: "              ",
+            point: point,
+            align: textAlign,
+            attributes: [
+                NSBackgroundColorAttributeName: marker.backgroundColor,
+                NSFontAttributeName: markerFont,
+                NSForegroundColorAttributeName: marker.textColor
+            ]
+        )
+        let text = marker.text ?? (yAxis.valueFormatter?.stringForValue(marker.value, axis: yAxis) ?? String(marker.value))
+        ChartUtils.drawText(
+            context: context,
+            text: text,
+            point: point,
+            align: textAlign,
+            attributes: [
+                NSBackgroundColorAttributeName: marker.backgroundColor,
+                NSFontAttributeName: markerFont,
+                NSForegroundColorAttributeName: marker.textColor
+            ]
+        )
+    }
 }
